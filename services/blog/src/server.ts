0e021929acd33ed17636f87816503aac120ed597
Saurav mishra <mishrasaurav499@gmail.com>
2026-01-08 15:48:50 +0530

    Blog service setup

---
+++ services/blog/src/server.ts
@@ -0,0 +1,15 @@
+import express from "express";
+import dotenv from "dotenv";
+import blogRoutes from "./routes/blog.js";
+
+dotenv.config();
+const app = express();
+
+const port = process.env.PORT;
+app.use(express.json());
+
+app.use("/api/blog", blogRoutes);
+
+app.listen(port, () => {
+  console.log(`✅ Server is running on port http://localhost:${port}`);
+});
