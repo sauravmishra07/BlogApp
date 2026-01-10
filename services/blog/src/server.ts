d1f1c052af7cd39b1f8437c0ca2aabd5c0e7f459
Saurav mishra <mishrasaurav499@gmail.com>
2026-01-10 15:13:56 +0530

    Controller funciton updated and redis connection implemented

--- services/blog/src/server.ts
+++ services/blog/src/server.ts
@@ -2,15 +2,44 @@ import express from "express";
 import dotenv from "dotenv";
 import blogRoutes from "./routes/blog.js";
 import cors from "cors";
+import { createClient } from "redis";
 
 dotenv.config();
-const app = express();
 
-const port = process.env.PORT;
+const app = express();
+const port = process.env.PORT || 3000;
+
 app.use(express.json());
+app.use(cors());
 
 app.use("/api/blog", blogRoutes);
 
-app.listen(port, () => {
-  console.log(`✅ Server is running on port http://localhost:${port}`);
+export const redisClient = createClient({
+  url: process.env.REDIS_URI as string,
+  socket: {
+    tls: true,
+    rejectUnauthorized: false,
+  }, 
 });
+
+redisClient.on("error", (err) => {
+  console.error("❌ Redis error:", err.message);
+});
+
+async function startServer() {
+  try {
+    await redisClient.connect();
+    console.log("✅ Upstash Redis connected");
+
+    await redisClient.set("foo", "bar");
+
+    app.listen(port, () => {
+      console.log(`✅ Server running at http://localhost:${port}`);
+    });
+  } catch (err) {
+    console.error("❌ Failed to connect to Upstash:", err);
+    process.exit(1);
+  }
+}
+
+startServer();
