003765969ca336762d34d6da1b0529ffe1ef80ee
Saurav mishra <mishrasaurav499@gmail.com>
2026-01-15 13:05:37 +0530

    Google OAuth setup in user Service login controller

--- services/user/src/controllers/user.ts
+++ services/user/src/controllers/user.ts
@@ -5,13 +5,33 @@ import TryCatch from "../utils/TryCatch.js";
 import type { AuthenticatedRequest } from "../middleware/isAuth.js";
 import getBuffer from "../utils/dataUri.js";
 import { v2 as cloudinary } from "cloudinary";
+import { oauth2client } from "../utils/GoogleConfig.js";
+import axios from "axios";
 
 export const loginUser = TryCatch(async (req, res) => {
-  const { email, name, image } = req.body;
+  const { code } = req.body;
+
+  if (!code) {
+    res.status(400).json({
+      message: "❌ Authorization code is required",
+    });
+    return;
+  }
+
+  const googleRes = await oauth2client.getToken(code);
+
+  
+  oauth2client.setCredentials(googleRes.tokens);
+  
+  const userRes = await axios.get(
+    `https://www.googleapis.com/oauth2/v2/userinfo?access_token=${googleRes.tokens.access_token}`
+  )
+
+  const { email, name, picture } = userRes.data;
 
   let user = await User.findOne({ email });
   if (!user) {
-    user = await User.create({ name, email, image });
+    user = await User.create({ name, email, image: picture });
   }
 
   const token = jwt.sign({ user }, process.env.JWT_SECRET as string, {
